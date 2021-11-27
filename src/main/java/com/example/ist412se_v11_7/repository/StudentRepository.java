793811a0a0b8a94741be49d8e18d6170ac026f7c
ffm5113 <73726304+ffm5113@users.noreply.github.com>
2021-11-27 14:48:00 -0800

    Update StudentRepository.java

--- src/main/java/com/example/ist412se_v11_7/repository/StudentRepository.java
+++ src/main/java/com/example/ist412se_v11_7/repository/StudentRepository.java
@@ -4,6 +4,6 @@ import com.example.ist412se_v11_7.model.Student; // Import student model class
 import org.springframework.data.jpa.repository.JpaRepository; // For StudentRepository class to extend JpaRepository
 import org.springframework.stereotype.Repository; // Used in the creation of a course repository
 
-@Repository // Create course repository via Spring framework stereotype
+@Repository // Create student repository via Spring framework stereotype
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
